2f421fb5dbd6ccdf9efca2e029beb52097f9e556
sejincp <sagela127@gmail.com>
2025-02-14 15:01:59 -0800

    add signup page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
