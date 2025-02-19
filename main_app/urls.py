f4c818354ae0aafb451e38d4e59b41913de2b68f
Max Himmel <himmelmax@gmail.com>
2025-02-19 14:57:02 -0800

    minor route reorganization

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('parties/', views.party_index, name='party-index'),
     path('parties/create/', views.PartyCreate.as_view(), name='party-create'),
     path('parties/find/', views.party_find, name='party-find'),
+    path('parties/<str:invite_id>/', views.party_detail, name='party-detail'),
     path('parties/<str:invite_id>/update/', views.PartyUpdate.as_view(), name='party-update'),
     path('parties/<str:invite_id>/add-rsvp/', views.add_rsvp, name='add-rsvp'),
     path('parties/<str:invite_id>/dishes/create/', views.DishCreate.as_view(), name='dish-create'),
     path('parties/<str:invite_id>/dishes/<int:pk>/update/', views.DishUpdate.as_view(), name='dish-update'),
-    path('parties/<str:invite_id>/', views.party_detail, name='party-detail'),
 ] 
