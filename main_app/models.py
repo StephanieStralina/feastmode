e1a8502600d56bf1d210136252463eaa116f15b6
Max Himmel <himmelmax@gmail.com>
2025-02-20 09:43:13 -0800

    move rsvp/party association into rsvp

--- main_app/models.py
+++ main_app/models.py
@@ -23,23 +23,11 @@ PARTY_STATUS = (
   ('C', 'Cancelled')
 )
 
-class Rsvp(models.Model):
-  status = models.CharField(
-    max_length=1,
-    choices=RSVP_STATUS,
-    default=RSVP_STATUS[0][0],
-  )
-  user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
-  def __str__(self):
-    return f"{self.status} - {self.user.username}"
-
   
 class Party(models.Model):
   name = models.CharField(max_length=100)
   owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
   invite_id = models.CharField(max_length=6, unique=True, blank=True, null=True)
-  rsvp = models.ManyToManyField(Rsvp, blank=True)
   time = models.DateField()
   location = models.CharField(max_length=150)
   dresscode = models.CharField(max_length=100)
@@ -61,6 +49,18 @@ class Party(models.Model):
   def get_absolute_url(self):
     return reverse('party-detail', kwargs={ 'invite_id': self.invite_id })
 
+class Rsvp(models.Model):
+  party = models.ForeignKey(Party, on_delete=models.CASCADE)
+  status = models.CharField(
+    max_length=1,
+    choices=RSVP_STATUS,
+    default=RSVP_STATUS[0][0],
+  )
+  user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+  def __str__(self):
+    return f"{self.status} - {self.user.username}"
+  
 class Dish(models.Model):
   party = models.ForeignKey(Party, on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
