f3b84638a697fb4afdbdb2f9bf8a93b195d28e56
sejincp <sagela127@gmail.com>
2025-02-20 14:01:28 -0800

    dish detail, home page revision, party form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Rsvp
+from .models import Rsvp, Party
+from django.forms import DateTimeInput
 
 class RsvpForm(forms.ModelForm):
     class Meta:
         model = Rsvp
         fields = ['status']
+
+class PartyForm(forms.ModelForm):
+    class Meta:
+        model = Party
+        fields = ['name', 'time', 'location', 'dresscode']
+        widgets = {
+            'time': forms.DateTimeInput(
+            # format=('%Y-%m-%d %H:%M'),
+            attrs={
+                'placeholder': 'Select a date',
+                'type': 'datetime-local'
+            }
+        )
+    }
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('accounts/signin/', views.Signin.as_view(), name='signin'),
     path('accounts/signup/', views.signup, name='signup'),
     path('parties/', views.party_index, name='party-index'),
     path('parties/create/', views.PartyCreate.as_view(), name='party-create'),
